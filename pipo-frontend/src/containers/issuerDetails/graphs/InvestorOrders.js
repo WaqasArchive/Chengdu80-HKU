057d928f9c12d2b0d95a0f11207cf5fbe04e03df
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2018-11-02 15:49:42 +0800

    json added to graph

--- pipo-frontend/src/containers/issuerDetails/graphs/InvestorOrders.js
+++ pipo-frontend/src/containers/issuerDetails/graphs/InvestorOrders.js
@@ -7,35 +7,7 @@ VictoryLine,
 VictoryBrushContainer,
 VictoryAxis,
 Bar } from 'victory';
-//import * as myDATA from './csvjson.json';
-
-const myDATA = [
-  {y: 45.52, x: 28},
-  {y: 47.11, x: 31},
-  {y: 23.25, x: 41},
-  {y: 47.11, x: 31},
-  {y: 19.82, x: 93},
-  {y: 43.98, x: 76},
-  {y: 22.72, x: 89},
-  {y: 25.61, x: 52},
-  {y: 26.99, x: 16},
-  {y: 21.17, x: 57},
-  {y: 47.37, x: 73},
-  {y: 24.68, x: 48},
-  {y: 45.83, x: 34},
-  {y: 26.66, x: 18},
-  {y: 41.89, x: 42}
-];
-
-const yDomain = myDATA.reduce(
-  (res, row) => {
-    return {
-      max: Math.max(res.max, row.y),
-      min: Math.min(0)
-    };
-  },
-  {max: -Infinity, min: Infinity}
-);
+import data from './data.js';
 
 export default class App extends React.Component {
   constructor() {
@@ -59,24 +31,18 @@ export default class App extends React.Component {
         }
       });
     };
-
     return (
       <div>
         <VictoryChart height={400} width={400}
           domainPadding={{ x: 50, y: [0, 20] }}
-          scale={{ x: "time" }}
+          scale={{ x: "ordinal" }}
         >
           <VictoryBar
             dataComponent={
               <Bar events={{ onMouseOver: handleMouseOver }}/>
             }
             style={this.state.style}
-            data={[
-              { x: new Date(1986, 1, 1), y: 2 },
-              { x: new Date(1996, 1, 1), y: 3 },
-              { x: new Date(2006, 1, 1), y: 5 },
-              { x: new Date(2016, 1, 1), y: 4 }
-            ]}
+            data={data.map(item => ({x: item.p, y: item.q}))}
           />
            <VictoryAxis
               label="Bidding Price"
